59ca733ec0ac4d89bbb413567f3d005fb5b959ac
saja amer <sajaamer86@gmail.com>
2019-09-16 14:16:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+let newHeader = document.createElement('h1') ;
+let newHeader=document.body.appendChild('h1#victory')
+let newheader = document.getElementById("h1#victory")
+newheader.innerHTML ="YOUR-NAME is the champion"
